80a7d108743e5487629e1201c0853d3db6f30bdc
Voggastur <v3x0r_x014@hotmail.com>
2020-08-09 22:43:57 +0000

    setup auto heroku deploy

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'htc^(xu7_1*mmi7&8)knt4@pa@u7jvv%u6por=5j(49qp**^nt')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
